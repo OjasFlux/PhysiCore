2ebfdbaf6bc47a7e9a703103682e0587cc7d94e8
SUJAN <155637883+OjasFlux@users.noreply.github.com>
2026-07-27 13:32:33 +0530

    Update piezo_test.ino

--- testing/piezo_test.ino
+++ testing/piezo_test.ino
@@ -1,1 +1,14 @@
-piezo_test.ino
+const int piezoPin = A0;
+
+void setup() {
+  Serial.begin(115200);
+}
+
+void loop() {
+  int value = analogRead(piezoPin);
+
+  Serial.print("Piezo = ");
+  Serial.println(value);
+
+  delay(10);
+}

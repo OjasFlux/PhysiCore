ad9ae1b43d2a9193be1fa80c8a73e74e3fbdd02d
SUJAN <155637883+OjasFlux@users.noreply.github.com>
2026-08-05 15:44:32 +0530

    Refactor MPU6050 test to include timestamps
    Added timestamp to MPU6050 readings and removed unnecessary prints.

--- testing/MPU6050/mpu6050_test.ino
+++ testing/MPU6050/mpu6050_test.ino
@@ -4,46 +4,42 @@
 
 Adafruit_MPU6050 mpu;
 
+unsigned long startTime;
+
 void setup() {
   Serial.begin(115200);
 
-  while (!Serial);
-
-  Serial.println("MPU6050 Test");
-
   if (!mpu.begin()) {
-    Serial.println("MPU6050 NOT Found!");
     while (1);
   }
 
-  Serial.println("MPU6050 Connected!");
+  startTime = millis();
 }
 
 void loop() {
-
   sensors_event_t a, g, temp;
 
   mpu.getEvent(&a, &g, &temp);
 
-  Serial.print("Accel X: ");
+  Serial.print(millis() - startTime);
+  Serial.print(",");
+
   Serial.print(a.acceleration.x);
+  Serial.print(",");
 
-  Serial.print("  Y: ");
   Serial.print(a.acceleration.y);
+  Serial.print(",");
 
-  Serial.print("  Z: ");
-  Serial.println(a.acceleration.z);
+  Serial.print(a.acceleration.z);
+  Serial.print(",");
 
-  Serial.print("Gyro X: ");
   Serial.print(g.gyro.x);
+  Serial.print(",");
 
-  Serial.print("  Y: ");
   Serial.print(g.gyro.y);
+  Serial.print(",");
 
-  Serial.print("  Z: ");
   Serial.println(g.gyro.z);
 
-  Serial.println("------------------------");
-
-  delay(500);
+  delay(20);
 }
